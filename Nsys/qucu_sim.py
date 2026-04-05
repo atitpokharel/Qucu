103a8c4ea7f75899b6930cd560875d7872c251f0
atitpokharel <ap1284@uah.edu>
2026-04-05 17:53:59 -0500

    memory management optimized

---
+++ Nsys/qucu_sim.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.append("/home/ap1284@DS.UAH.edu/Atit/GPU_Computing/Qucu")
+
+import qucu
+import numpy as np
+
+class QuCu:
+    def __init__(self, n_qubits):
+        self.n_qubits       = n_qubits
+        self._sim           = qucu.QuCuSim(n_qubits) # persistent device state
+        self.kernel_time_ms = 0.0
+
+    def reset(self):
+        self._sim.reset()# single H2D transfer
+        self.kernel_time_ms = 0.0
+        self._sim.kernel_time_ms = 0.0
+
+    def rx(self, target, theta):
+        self.kernel_time_ms += self._sim.rx(target, theta)
+
+    def ry(self, target, theta):
+        self.kernel_time_ms += self._sim.ry(target, theta)
+
+    def rz(self, target, theta):
+        self.kernel_time_ms += self._sim.rz(target, theta)
+
+    def cnot(self, control, target):
+        self.kernel_time_ms += self._sim.cnot(control, target)
+
+    def measure(self):
+        return self._sim.measure()# single D2H transfer
